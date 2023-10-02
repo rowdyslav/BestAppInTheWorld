9b0eb756486612de0c375c8b7249899e4f71a7e5
rowdyslav <rowdyslav@gmail.com>
2023-10-02 23:00:09 +0300

    PIZDEC

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from flask import Flask
-from roles import User
-from db import USERS
-from pprint import pprint
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def main():
-    return render_template('templates/main.html')
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
