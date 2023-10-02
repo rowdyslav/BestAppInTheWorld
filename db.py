cb5fd5e8436310aeef02245d80f8ece2267951a6
rowdyslav <rowdyslav@gmail.com>
2023-10-02 16:49:43 +0300

    db

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-DB_URL = getenv("MONGODB_URL")
+DB_URL = "mongodb+srv://rowdyslav:228doxy228@cluster0.736skbi.mongodb.net/?retryWrites=true&w=majority"
 CLIENT = MongoClient(DB_URL, server_api=ServerApi("1"))
-DB = CLIENT[config["db_name"]]
-COLLECTION = DB[config["coll_name"]]
+DB = CLIENT["CubeFood"]
+USERS = DB["users"]
