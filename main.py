95b3ec0d23c843f23a63d3b3669817ab02366433
rowdyslav <rowdyslav@gmail.com>
2023-10-02 17:03:06 +0300

    ssss

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from users import User
+from db import USERS
+from pprint import pprint
 
 
 def main():
     print("HelloWorld!")
+    a = USERS.find_one({"login": "admin"})
+    pprint(a)
 
 
 if __name__ == "__main__":
