be94545863a5a9116604c00dc23ccbfb932bab5b
rowdyslav <rowdyslav@gmail.com>
2023-10-02 18:09:45 +0300

    db loader and roles

--- roles.py
+++ roles.py
@@ -1,5 +1,12 @@
 class Role:
-    pass
+    def __init__(self, fio) -> None:
+        self.fio = fio
+
+    def __str__(self) -> str:
+        return self.fio
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}{vars(self)}"
 
 
 class User(Role):
