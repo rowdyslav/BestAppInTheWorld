85a5f7abe9d6df0fb800fd241fb0d8e3343414af
rowdyslav <rowdyslav@gmail.com>
2023-10-03 13:29:36 +0300

    fixes

--- db_loader.py
+++ db_loader.py
@@ -1,5 +1,5 @@
 from db import USERS
-from roles import User, Admin, Cooker
+from classes import User, Admin, Cooker
 from pprint import pprint
 
 CLASSES = {"user": User, "admin": Admin, "cooker": Cooker}
