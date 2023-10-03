f69e86964ac00ce2351678b8bf4a780930ed8d52
rowdyslav <rowdyslav@gmail.com>
2023-10-03 16:44:12 +0300

    auth + roles

--- classes/roles.py
+++ classes/roles.py
@@ -4,8 +4,9 @@ from classes.utils import Office
 
 
 class User:
-    def __init__(self, email, fio) -> None:
+    def __init__(self, email, password, fio) -> None:
         self.email = email
+        self.password = password
         self.fio = fio
 
     def __str__(self) -> str:
@@ -21,8 +22,8 @@ class OfficeWorker(User):
 
 
 class OfficeManager(OfficeWorker):
-    def __init__(self, email, fio, office: Office) -> None:
-        super().__init__(email, fio)
+    def __init__(self, email, password, fio, office: Office) -> None:
+        super().__init__(email, password, fio)
         self.own_office = office
 
     def add_office_worker(self, user_email) -> None:
@@ -36,8 +37,7 @@ class RestaurantAdmin(User):
     """Должна быть логика добавения оффиса со своим менеджером"""
 
     def add_office(self, user_email, address="Адрес оффиса по умолчанию") -> None:
-        OFFICES.insert_one(Office(user_email, address))
+        OFFICES.insert_one(vars(Office(user_email, address)))
 
-    def remove_office(self, office_id) -> None:
-        OFFICES.find_one_and_delete({'id': office_id})
-    
+    def remove_office(self, office_name) -> None:
+        OFFICES.find_one_and_delete({'name': office_name})
