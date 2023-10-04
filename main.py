a3f5a058853acfb880c55c3bdc820ff269fea263
danilnev <danilanevinsky@gmail.com>
2023-10-04 08:17:52 +0300

    admin account created

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_session import Session
 from cogs.authentication import registration, login
 from cogs.utils import login_required
 
-from db_connector import USERS
+from db_connector import USERS, OFFICES
 
 
 app = Flask(__name__)
@@ -38,7 +38,12 @@ def log():
     result = login(email, password)
     if result[1]:
         session["email"] = email
-        return redirect(url_for("account", email=email))
+        if result[2] == 'user':
+            return redirect(url_for('account', email=email))
+        elif result[2] == 'admin':
+            return redirect(url_for('admin_account', email=email))
+        elif result[2] == 'cooker':
+            return redirect(url_for('cooker_account', email=email))
     else:
         return render_template('index.html', error_msg=result[0])
 
@@ -55,6 +60,23 @@ def account():
 def exit():
     session.pop('email', None)
     return redirect(url_for("index", error_msg=None))
+
+
+@app.route("/admin_account")
+@login_required
+def admin_account():
+    email = request.args.get("email")
+    admin = USERS.find_one({"email": email})
+    address = OFFICES.find_one({'manager_email': email})['address']
+    return render_template("admin_account.html", admin=admin, address=address)
+
+
+@app.route("/cooker_account")
+@login_required
+def cooker_account():
+    email = request.args.get("email")
+    cooker = USERS.find_one({"email": email})
+    return render_template("cooker_account.html", cooker=cooker)
 
 
 if __name__ == "__main__":
