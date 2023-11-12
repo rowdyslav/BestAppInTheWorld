fa5ec29b1cabe6b9ef51899f9bb642323f9bdb1e
rowdyslav <rowdyslav@gmail.com>
2023-11-12 13:13:37 +0300

    s

--- db_connector.py
+++ db_connector.py
@@ -1,6 +1,10 @@
 from pymongo.mongo_client import MongoClient
+from icecream import ic
 
 DB_URL = "mongodb+srv://rowdyslav:228doxy228@cluster0.736skbi.mongodb.net/?retryWrites=true&w=majority"
+
+print("Подключение к MongoDB...")
+
 DB_CLIENT = MongoClient(DB_URL)
 CUBEFOOD_DB = DB_CLIENT["CubeFood"]
 
