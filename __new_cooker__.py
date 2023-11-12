c9fd3c752e7307f43a8f58f3642e953a6f6fb215
rowdyslav <rowdyslav@gmail.com>
2023-11-12 15:27:58 +0300

    s

---
+++ __new_cooker__.py
@@ -0,0 +1,17 @@
+from roles import Base
+from sys import argv
+
+fields = ["Логин", "Пароль", "ФИО"]
+cmd_args = {x[0]: x[1] for x in enumerate(argv)}
+
+data = []
+for i, field in enumerate(fields, 1):
+    value = cmd_args.get(i)
+    if value:
+        print(f"{field} нового кукера > {value}")
+    else:
+        value = input(f"{field} нового кукера > ")
+    data.append(value)
+
+login, password, fio = data
+print(Base(login, password, fio)._registration("cooker"))
--- add_cooker.py
+++
@@ -1,7 +0,0 @@
-from roles import Base
-
-login = input("Логин нового кукера > ")
-password = input("Пароль нового кукера > ")
-fio = input("ФИО нового кукера > ")
-
-print(Base(login, password, fio)._registration("cooker"))
