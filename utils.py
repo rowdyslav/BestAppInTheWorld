d66b4734503683515f23cfa0df2597237f5cffe8
Yandex Lyceum <kondrasov516@gmail.com>
2023-11-21 10:51:34 +0300

    _set_role  in utils

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ import functools
 
 from db_conn import USERS
 
+from roles import ROLES_NAMES
+
 
 def _is_login_free(login: str) -> bool:
     user_with_login = USERS.find_one({"login": login})
@@ -22,3 +24,15 @@ def _role_required(role: type):
         return secure_function
 
     return decorator
+
+
+def _set_role(user_login: str, role: str):
+    q = {"login": user_login}
+
+    worker = USERS.find_one(q)
+    if not worker:
+        return "Сотрудник не найден!"
+    
+    USERS.update_one(q, {"$set": {"role": role}})
+
+    return "Роль успешно выдана!"
