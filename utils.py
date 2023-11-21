0de6640792eb4c537fac6b952f9e4f4a450f137d
rowdyslav <rowdyslav@gmail.com>
2023-11-21 11:33:05 +0300

    s

--- utils.py
+++ utils.py
@@ -3,8 +3,6 @@ import functools
 
 from db_conn import USERS
 
-from roles import ROLES_NAMES
-
 
 def _is_login_free(login: str) -> bool:
     user_with_login = USERS.find_one({"login": login})
@@ -32,7 +30,7 @@ def _set_role(user_login: str, role: str):
     user = USERS.find_one(q)
     if not user:
         return "Сотрудник не найден!"
-    
+
     USERS.update_one(q, {"$set": {"role": role}})
 
     return "Роль успешно выдана!"
