c7332650c0509e6adf6cd8967d6b6e6884eacf19
rowdyslav <rowdyslav@gmail.com>
2023-11-24 14:29:28 +0500

    dwdwwd

--- tg_bot.py
+++ tg_bot.py
@@ -21,6 +21,6 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda _: True)
 def echo_message(message):
     bot.reply_to(message, message.text)
