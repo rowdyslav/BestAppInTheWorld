dca3d17171a1a7e27416e01d3fa999a87dd7418e
rowdyslav <rowdyslav@gmail.com>
2023-11-25 11:59:15 +0500

    swdwdwdwdw

--- website.py
+++ website.py
@@ -77,9 +77,9 @@ def account():
 
         case Deliverier():
             deliverier = USERS.find_one({"login": session["user"].login})
-            orders = ORDERS.find({'deliverier': session["user"].login})
-            for order in orders:
-                order['date'] = dt.strftime(order['date'], '%d/%m/%Y') 
+            orders = list(ORDERS.find({'deliverier': session["user"].login}))
+            for ind, order in enumerate(orders):
+                orders[ind]['date'] = dt.strftime(order['date'], '%d/%m/%Y') 
 
             context = {"deliverier": deliverier, 'orders': orders}
 
